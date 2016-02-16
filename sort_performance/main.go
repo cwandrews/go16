4e843d183ff87cb7461bb8749720c68083af300a
Trevor Rosen <trevor@catapult-creative.com>
2016-02-16 08:52:51 -0600

    Add a bit of commentary about the sort.Interface

--- sort_performance/main.go
+++ sort_performance/main.go
@@ -11,6 +11,8 @@ func (p People) String() string { return p.first + " " + p.last }
 
 type ByFirstName []People
 
+// Here we implement the interface described by the sort.Interface type for
+// ByFirstName, making our custom type sortable. https://golang.org/pkg/sort/#Interface
 func (s ByFirstName) Len() int           { return len(s) }
 func (s ByFirstName) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s ByFirstName) Less(i, j int) bool { return s[i].first < s[j].first }
